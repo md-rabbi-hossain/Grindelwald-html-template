c09aa8bd61c337783db4c34903ab2a3195fb14bb
Md Rabbi Hossain <mdrabbihossain1443@gmail.com>
2024-07-08 00:00:08 +0600

    slider-done

---
+++ js/script.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".center-slider");
+  const slides = document.querySelectorAll(".slide");
+  const prevButton = document.getElementById("prev");
+  const nextButton = document.getElementById("next");
+  let currentIndex = 0;
+  const slidesToShow = 3;
+  const totalSlides = slides.length;
+
+  function cloneSlides() {
+    for (let i = 0; i < slidesToShow; i++) {
+      let cloneFirst = slides[i].cloneNode(true);
+      let cloneLast = slides[totalSlides - 1 - i].cloneNode(true);
+      slider.appendChild(cloneFirst);
+      slider.insertBefore(cloneLast, slider.firstChild);
+    }
+  }
+  cloneSlides();
+  const newTotalSlides = slider.children.length;
+  function updateSlider() {
+    slider.style.transition = "transform 0.4s ease-in-out";
+    slider.style.transform = `translateX(${
+      -(currentIndex + slidesToShow) * (100 / slidesToShow)
+    }%)`;
+  }
+  function nextSlide() {
+    if (currentIndex >= totalSlides) {
+      slider.style.transition = "none";
+      currentIndex = 0;
+      slider.style.transform = `translateX(${
+        -(currentIndex + slidesToShow) * (100 / slidesToShow)
+      }%)`;
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          currentIndex++;
+          updateSlider();
+        });
+      });
+    } else {
+      currentIndex++;
+      updateSlider();
+    }
+  }
+  function prevSlide() {
+    if (currentIndex <= 0) {
+      slider.style.transition = "none";
+      currentIndex = totalSlides;
+      slider.style.transform = `translateX(${
+        -(currentIndex + slidesToShow) * (100 / slidesToShow)
+      }%)`;
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          currentIndex--;
+          updateSlider();
+        });
+      });
+    } else {
+      currentIndex--;
+      updateSlider();
+    }
+  }
+  nextButton.addEventListener("click", nextSlide);
+  prevButton.addEventListener("click", prevSlide);
+
+  setInterval(nextSlide, 5000);
+  slider.style.transform = `translateX(${
+    -(currentIndex + slidesToShow) * (100 / slidesToShow)
+  }%)`;
+});
